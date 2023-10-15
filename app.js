0872d706e96c028c49775b720f0909000a26b10b
IsakMakimartti <t2mais00@students.oamk.fi>
2023-10-15 16:51:20 +0300

    modiofied app.js and layerfile

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
  
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -11,4 +11,20 @@ const server = http.createServer((req, res) => {
  
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+});*/
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Test Route!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });

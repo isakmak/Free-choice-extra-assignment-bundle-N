0898a3ba7bd25ee492f4d46aea9cfc8b4d259320
IsakMakimartti <t2mais00@students.oamk.fi>
2023-10-15 19:20:14 +0300

    added app.js and layerfile

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+var http = require('http');
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.get("/test", (req, res) => {
-  res.send("Test Route!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(3000); 
